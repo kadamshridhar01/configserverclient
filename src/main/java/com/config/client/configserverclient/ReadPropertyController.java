86e119336e103896fae3faa2919e9be3f91f0932
kadamshridhar01 <36438544+kadamshridhar01@users.noreply.github.com>
2018-12-26 12:39:50 +0530

    formatting

--- src/main/java/com/config/client/configserverclient/ReadPropertyController.java
+++ src/main/java/com/config/client/configserverclient/ReadPropertyController.java
@@ -5,16 +5,16 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 @RefreshScope
+@RestController
+
 public class ReadPropertyController {
+	
 	@Value("${spring.datasource.url: url not found}")
 	private String configMessage;
 	
-	@GetMapping("/")
-	public String welcome() {
-		return "welcome shridhar";
-	}
+	
 	@GetMapping("/${server.contextPath}")
 	public String jdbcUrl() {
 		return configMessage;
